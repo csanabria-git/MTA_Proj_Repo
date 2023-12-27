92b227e531632212aece8f0f161fb01a34dbbc3c
csanabria-git <csanabria2013@live.com>
2023-12-27 15:59:54 -0500

    Css test

--- flask/ParseTrains_app.py
+++ flask/ParseTrains_app.py
@@ -91,9 +91,7 @@ for i in CommandDict:
 
 #Flask web app
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route('/')
 def my_route():
   return render_template('index.html', trains=MasterDict)
